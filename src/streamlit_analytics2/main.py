f460523ed5fb7f5d40f97777207fdc68101c89dd
444B <15311579+444B@users.noreply.github.com.>
2024-12-30 21:41:53 +0000

    working config - tested

--- src/streamlit_analytics2/main.py
+++ src/streamlit_analytics2/main.py
@@ -18,66 +18,13 @@ from .state import data, reset_data
 
 # from streamlit_searchbox import st_searchbox
 
-
-logging.basicConfig(
-    level=logging.INFO, format="streamlit-analytics2: %(levelname)s: %(message)s"
-)
+# TODO look into https://github.com/444B/streamlit-analytics2/pull/119 to
+# integrate
+# logging.basicConfig(
+#     level=logging.INFO, format="streamlit-analytics2: %(levelname)s: %(message)s"
+# )
 # Uncomment this during testing
-logging.info("SA2: Streamlit-analytics2 successfully imported")
-
-
-reset_data()
-
-
-# widgets.copy_original()
-
-# Store original streamlit functions. They will be monkey-patched with some wrappers
-# in `start_tracking` (see wrapper functions below).
-_orig_button = st.button
-_orig_checkbox = st.checkbox
-_orig_radio = st.radio
-_orig_selectbox = st.selectbox
-_orig_multiselect = st.multiselect
-_orig_slider = st.slider
-_orig_select_slider = st.select_slider
-_orig_text_input = st.text_input
-_orig_number_input = st.number_input
-_orig_text_area = st.text_area
-_orig_date_input = st.date_input
-_orig_time_input = st.time_input
-_orig_file_uploader = st.file_uploader
-_orig_color_picker = st.color_picker
-# new elements, testing
-# _orig_download_button = st.download_button
-# _orig_link_button = st.link_button
-# _orig_page_link = st.page_link
-# _orig_toggle = st.toggle
-# _orig_camera_input = st.camera_input
-_orig_chat_input = st.chat_input
-# _orig_searchbox = st_searchbox
-
-
-_orig_sidebar_button = st.sidebar.button
-_orig_sidebar_checkbox = st.sidebar.checkbox
-_orig_sidebar_radio = st.sidebar.radio
-_orig_sidebar_selectbox = st.sidebar.selectbox
-_orig_sidebar_multiselect = st.sidebar.multiselect
-_orig_sidebar_slider = st.sidebar.slider
-_orig_sidebar_select_slider = st.sidebar.select_slider
-_orig_sidebar_text_input = st.sidebar.text_input
-_orig_sidebar_number_input = st.sidebar.number_input
-_orig_sidebar_text_area = st.sidebar.text_area
-_orig_sidebar_date_input = st.sidebar.date_input
-_orig_sidebar_time_input = st.sidebar.time_input
-_orig_sidebar_file_uploader = st.sidebar.file_uploader
-_orig_sidebar_color_picker = st.sidebar.color_picker
-# _orig_sidebar_searchbox = st.sidebar.st_searchbox
-# new elements, testing
-# _orig_sidebar_download_button = st.sidebar.download_button
-# _orig_sidebar_link_button = st.sidebar.link_button
-# _orig_sidebar_page_link = st.sidebar.page_link
-# _orig_sidebar_toggle = st.sidebar.toggle
-# _orig_sidebar_camera_input = st.sidebar.camera_input
+# logging.info("SA2: Streamlit-analytics2 successfully imported")
 
 
 def update_session_stats(data_dict: Dict[str, Any]):
@@ -493,4 +440,55 @@ def track(
 
 
 if __name__ == "streamlit_analytics2.main":
-    print("hello world!")
+    reset_data()
+
+    # widgets.copy_original()
+    # TODO need to fix the scope for this function call and then we can move
+    # these variable assignments to widgets.py
+
+    # Store original streamlit functions. They will be monkey-patched with some wrappers
+    # in `start_tracking` (see wrapper functions below).
+    _orig_button = st.button
+    _orig_checkbox = st.checkbox
+    _orig_radio = st.radio
+    _orig_selectbox = st.selectbox
+    _orig_multiselect = st.multiselect
+    _orig_slider = st.slider
+    _orig_select_slider = st.select_slider
+    _orig_text_input = st.text_input
+    _orig_number_input = st.number_input
+    _orig_text_area = st.text_area
+    _orig_date_input = st.date_input
+    _orig_time_input = st.time_input
+    _orig_file_uploader = st.file_uploader
+    _orig_color_picker = st.color_picker
+    # new elements, testing
+    # _orig_download_button = st.download_button
+    # _orig_link_button = st.link_button
+    # _orig_page_link = st.page_link
+    # _orig_toggle = st.toggle
+    # _orig_camera_input = st.camera_input
+    _orig_chat_input = st.chat_input
+    # _orig_searchbox = st_searchbox
+
+    _orig_sidebar_button = st.sidebar.button
+    _orig_sidebar_checkbox = st.sidebar.checkbox
+    _orig_sidebar_radio = st.sidebar.radio
+    _orig_sidebar_selectbox = st.sidebar.selectbox
+    _orig_sidebar_multiselect = st.sidebar.multiselect
+    _orig_sidebar_slider = st.sidebar.slider
+    _orig_sidebar_select_slider = st.sidebar.select_slider
+    _orig_sidebar_text_input = st.sidebar.text_input
+    _orig_sidebar_number_input = st.sidebar.number_input
+    _orig_sidebar_text_area = st.sidebar.text_area
+    _orig_sidebar_date_input = st.sidebar.date_input
+    _orig_sidebar_time_input = st.sidebar.time_input
+    _orig_sidebar_file_uploader = st.sidebar.file_uploader
+    _orig_sidebar_color_picker = st.sidebar.color_picker
+    # _orig_sidebar_searchbox = st.sidebar.st_searchbox
+    # new elements, testing
+    # _orig_sidebar_download_button = st.sidebar.download_button
+    # _orig_sidebar_link_button = st.sidebar.link_button
+    # _orig_sidebar_page_link = st.sidebar.page_link
+    # _orig_sidebar_toggle = st.sidebar.toggle
+    # _orig_sidebar_camera_input = st.sidebar.camera_input
