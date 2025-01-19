a6233840de6605ae239caa07690046c81ccf70ce
444B <15311579+444B@users.noreply.github.com>
2025-01-19 19:05:47 +0000

    fixes #132

--- src/streamlit_analytics2/__init__.py
+++ src/streamlit_analytics2/__init__.py
@@ -5,6 +5,7 @@ Track & visualize user interactions with your streamlit app.
 
 from .main import start_tracking, stop_tracking, track  # noqa: F401
 from .state import data  # noqa: F401
+from .state import data as counts # noqa: F401
 
 __version__ = "0.10.2"
 __name__ = "streamlit_analytics2"
