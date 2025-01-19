4423c12955663f328aa8244bf16527bc1d054019
444B <15311579+444B@users.noreply.github.com>
2025-01-19 20:17:21 +0000

    bump and release bug fix 0.10.3

--- src/streamlit_analytics2/__init__.py
+++ src/streamlit_analytics2/__init__.py
@@ -8,5 +8,5 @@ from .state import data  # noqa: F401
 
 from .state import data as counts  # noqa: F401  # isort:skip
 
-__version__ = "0.10.2"
+__version__ = "0.10.3"
 __name__ = "streamlit_analytics2"
