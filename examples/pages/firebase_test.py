46cbe69038ca8c0769ca097c4edac5c5e97ed8bb
Dave Rench McCauley <11720378+emigre459@users.noreply.github.com>
2025-03-03 14:23:07 -0500

    Flesh out the advanced firebase_test page and tab a bit for future efforts

--- examples/pages/firebase_test.py
+++ examples/pages/firebase_test.py
@@ -29,10 +29,16 @@ with tab2:
     # if st.button("Test Advanced"):
 
     #     with streamlit_analytics.track(
-    #         firestore_collection_name="counts",
+    #         firestore_collection_name="streamlit-analytics-secrets-approach",
     #         streamlit_secrets_firestore_key="firebase",
     #         firestore_project_name="firestore_project_name",
+    #         session_id="test_session_id",
     #         verbose=True):
 
-    #         st.text_input("Write something")
-    #         st.button("Click me")
+    #         st.text_input("Write something", key="text_input2")
+    #         st.button("Click me", key="button2")
+
+    #         st.write("# Aggregate Stats")
+    #         st.write(streamlit_analytics.data)
+    #         st.write("# Session Stats")
+    #         st.write(streamlit_analytics.session_data)
